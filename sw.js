68578da8b2cb73185417522071b0a193cdcc3d10
ekiananta-project <ekiananta.project@proton.me>
2026-09-08 19:15:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "family-superapp-v1.1.7b-single-route-authority-1";
+const CACHE_NAME = "family-superapp-v1.1.7c-compact-finance-layers-1";
 const OFFLINE_URL = "./offline.html";
 
 const APP_FILES = [
@@ -17,6 +17,7 @@ const APP_FILES = [
   "./css/pages/dompet-backend.css",
   "./css/pages/dompet-detail.css",
   "./css/pages/dompet-detail.css?v=20260908-2",
+  "./css/pages/dompet-detail.css?v=20260908-3",
   "./css/pages/dompet-form.css",
   "./css/pages/family-invitation.css",
   "./css/pages/family-invitation.css?v=20260908-2",
