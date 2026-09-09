814e7d339681838b91e570ab406bd9f28bed37e2
ekiananta-project <ekiananta.project@proton.me>
2026-09-09 20:42:46 +0700

    Add files via upload

--- js/pwa.js
+++ js/pwa.js
@@ -43,7 +43,7 @@
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const registration = await navigator.serviceWorker.register("./sw.js?v=20260909-v200a15", {
+        const registration = await navigator.serviceWorker.register("./sw.js?v=20260909-v200a16", {
           scope: "./"
         });
 
